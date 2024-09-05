16cc61fed57af6cf5c7cd083291d601843d5cf3d
Mackenzie <Mackenzie-98>
2024-09-05 16:46:35 -0500

    pus fix

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://main.d2mwojyqore1ov.amplifyapp.com', // URL del frontend en Amplify
-    methods: 'GET,POST,PUT,DELETE',
+    origin: 'https://main.d2mwojyqore1ov.amplifyapp.com',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Incluye OPTIONS
     allowedHeaders: 'Content-Type, Authorization',
   });
+
   await app.listen(3000);
 
 }
