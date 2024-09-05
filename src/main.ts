468e0ae2c6e3b49bd05bd5d9ef97b2f41ccb2993
Mackenzie <Mackenzie-98>
2024-09-05 15:56:18 -0500

    prod updates

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://main.d2mwojyqore1ov.amplifyapp.com', // URL del frontend en Amplify
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
   });
-  app.enableCors();
   await app.listen(3000);
 
 }
