69e50e5a6481b35b10460b1176795627b2d39066
Mackenzie <Mackenzie-98>
2024-09-25 21:09:28 -0500

    Fix

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   });
 
   app.enableCors();
-  await app.listen(3000, '127.0.0.1', () => {
+  await app.listen(3000, '0.0.0.0', () => {
     console.log('Server is running on port 3000');
   });
 }
